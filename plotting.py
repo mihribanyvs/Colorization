eace92d8d030f02f17c553fe36ef6d9a47ed62f8
mihribanyvs <mrbn.yavas@gmail.com>
2025-02-14 22:34:42 +0100

    addind plotting function

---
+++ plotting.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+
+# Funzione per visualizzare un'immagine da un tensor
+def imshow(tensor_img, cmap=None, yuv = False):
+    # Converti il tensor da (C, H, W) a (H, W, C) per matplotlib
+    
+    img = tensor_img.numpy()
+    
+    if yuv == False:
+        img = tensor_img.permute(1, 2, 0).numpy()  # (C, H, W) -> (H, W, C)
+        plt.imshow(img)
+        plt.axis('off')  # Nasconde gli assi
+        plt.show()
+
+    else:
+        plt.imshow(img, cmap = cmap)
+        plt.axis('off')
+        plt.show()
